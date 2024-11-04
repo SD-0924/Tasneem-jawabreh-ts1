68abea12beac8fcc102c01fad8154fe48d7f949f
Taneem Jawabreh <tasneemkjawabreh@gmail.com>
2024-11-04 23:46:05 +0300

    test

---
+++ __tests__/resizeImage.test.ts
@@ -0,0 +1,21 @@
+import { resizeImage } from '../src/controllers/resizeImageController';
+import sharp from 'sharp';
+
+jest.mock('sharp');
+
+describe('Image Resize Function', () => {
+
+
+  it('should return an error when no image is uploaded', async () => {
+    const req: any = { file: null };
+    const res: any = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    await resizeImage(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'No image uploaded' });
+  });
+});
